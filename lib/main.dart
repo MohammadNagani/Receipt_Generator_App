0dfd3b8b9aa6afccf87a6a83eb04415d67db5b72
Mohammad Nagani <mohammadnagani123@gmail.com>
2025-06-22 18:24:25 +0530

    Update main.dart
    removed firebase acc

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://mrdcygimgskcwotkzztu.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yZGN5Z2ltZ3NrY3dvdGt6enR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1Nzg4MjMsImV4cCI6MjA2NjE1NDgyM30.tqcQ7J0BHfYZ9YDoQ_3gB_7iAegOgil46jSr50Flkdo',
+    url: 'url',
+    anonKey: 'key',
   );
   runApp(MyApp());
 }
